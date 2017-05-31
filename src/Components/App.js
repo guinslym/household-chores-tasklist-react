db239b39b6df3ba71b1aaa882cea70e43023c598
Clint Bush <clint@madebyclint.com>
2017-05-31 16:52:32 -0700

    Add basic dl for schedule tasks.

--- src/Components/App.js
+++ src/Components/App.js
@@ -3,6 +3,7 @@
 import React, { Component } from "react";
 import logo from "../Assets/logo.svg";
 import "./App.css";
+import Schedule from "./Schedule.js";
 
 class App extends Component {
   render() {
@@ -15,6 +16,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Schedule />
       </div>
     );
   }
---
+++ src/Components/Schedule.js
@@ -0,0 +1,51 @@
+// @flow
+
+import React, { Component, PureComponent } from "react";
+import PropTypes from "prop-types";
+// import "./App.css";
+
+const repeatElement = function(
+  el: string,
+  times: number,
+  content: string,
+  classes: ?Array<string>
+) {
+  let arr = [];
+  for (times > 0; times--; ) {
+    arr.push(
+      React.createElement(
+        el,
+        { className: classes && classes.join(" ") },
+        content
+      )
+      // <span key={times} className={classes && classes.join(" ")}>
+      //   {content}
+      // </span>
+    );
+  }
+  return arr;
+};
+
+let tasks = repeatElement("dd", 3, "Hello world", ["something", "another"]);
+console.log("tasks", tasks);
+
+class Schedule extends PureComponent {
+  render() {
+    return (
+      <div className="Schedule">
+        <div className="Schedule__header">
+          <h2>Weekly Schedule</h2>
+        </div>
+        <p className="Schedule__intro">
+          To get started, edit <code>src/Schedule.js</code> and save to reload.
+        </p>
+        <dl className="Schedule__list">
+          <dt>Brush kids teeth (morning)</dt>
+          {tasks}
+        </dl>
+      </div>
+    );
+  }
+}
+
+export default Schedule;
